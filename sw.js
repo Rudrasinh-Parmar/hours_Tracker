3df70c8ba91cbab5223cd122911152821034096f
ZORO <rudrasinhp2803@gmail.com>
2026-08-21 05:10:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'work-tracker-v1';
+const CACHE = 'work-tracker-v7';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
